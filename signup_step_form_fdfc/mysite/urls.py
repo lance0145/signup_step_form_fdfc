ed25cc11ecc69f1ee6c6df05352f4bf0ae42e3a5
lance0145 <48038456+lance0145@users.noreply.github.com>
2020-08-12 10:04:57 +0800

    Add files via upload

---
+++ signup_step_form_fdfc/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from mysite.core.forms import ContactForm1, ContactForm2, ContactForm3
+from mysite.core.views import ContactWizard
+from mysite.core import views as core_views
+
+
+urlpatterns = [
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    url(r'^contact/$', ContactWizard.as_view([ContactForm1, ContactForm2, ContactForm3]), name='contact'),
+]
